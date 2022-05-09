2199da977377d7f25f9103539c855dd6bb58742d
WilliamChidiac <94478368+WilliamChidiac@users.noreply.github.com>
2022-05-10 00:26:57 +0200

    Update and rename main.cpp to ppm-main.cpp

---
+++ ppm-main.cpp
@@ -0,0 +1,23 @@
+#include "Jeux.hpp"
+
+
+int main() {
+    Jeu a{};
+    int compteur;
+    for (compteur = 0; compteur < 700; compteur++) {
+        vector<int>ren = a.GetIdRenLap().first;
+        vector<int>lap = a.GetIdRenLap().second;
+        while(not lap.empty()){
+            int val = rand()%lap.size();
+            a.Deplace(lap[val]);
+            lap.erase(lap.begin()+val);
+        }
+        while(not ren.empty()){
+            int val = rand()%ren.size();
+            a.Deplace(ren[val]);
+            ren.erase(ren.begin()+val);
+        }
+        a.dessineGrille(compteur);
+    }
+    return 0;
+}
