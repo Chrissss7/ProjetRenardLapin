6ae41b92c26c90be0102081191bd66536ed5077f
WilliamChidiac <94478368+WilliamChidiac@users.noreply.github.com>
2022-05-09 23:41:49 +0200

    Update Ensemble.hpp

--- Ensemble.hpp
+++ Ensemble.hpp
@@ -6,12 +6,45 @@
 const int MAXcard = 8;
 struct Ensemble{
     friend std::ostream& operator<<(std::ostream&, Ensemble);
+    
+    //constuctors
+    
     Ensemble();
+    
+    //methodes
+    
+    /**
+     * @brief verifie si l'ensemble est vide
+     * 
+     * @return true si il est vide
+     * @return false sinon
+     */
     bool estVide()const;
+    
+    /**
+     * @brief 
+     * 
+     * @return le cardinal de l'ensemble
+     */
     int cardinal() const;
+    
+    /**
+     * @brief aujoute un element a l'ensemble si ce dernier n'est pas deja plein (cardinalmax = 8)
+     * 
+     * @param a l'element a rajoute
+     */
     void ajoute(int);
+    
+    /**
+     * @brief tirage aleatoire d'un element de l'ensemble si ce dernier n'est pas vide, l'element tire sera retire de l'ensemble
+     * 
+     * @return l'element choisi
+     */
     int Tire();
 private :
+    
+    //attributs 
+    
     std::array<int,MAXcard> tab;
     int card;
 };
