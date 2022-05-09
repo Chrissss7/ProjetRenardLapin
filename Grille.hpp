179fae10557fa3360f1023e843f9f570e8598c2d
WilliamChidiac <94478368+WilliamChidiac@users.noreply.github.com>
2022-05-09 23:45:02 +0200

    Update Grille.hpp

--- Grille.hpp
+++ Grille.hpp
@@ -5,12 +5,48 @@
 #include <array>
 using Plateau = std::array<std::array<int,TAILLE>,TAILLE>;
 struct Grille {
+    
+    //constructors 
+    
     Grille();
-    bool CaseVide(Coord ind) const{return GetId(ind) == -1;}
+    
+    /**
+     * @brief 
+     * 
+     * @param ind une coordonne donnees 
+     * @return la valeur dans la grille au coordonne de ind
+     */
     int GetId(Coord ind)const {return grille[ind.getX()][ind.getY()];}
-    void videCase(Coord);
-    void setCase(Coord, int);
+    
+    /**
+     * @brief verifie si la case a l'indice choisi est vide
+     * 
+     * @param ind les coordonne de la case a verifier
+     * @return true si la case est vide 
+     * @return false sinon
+     */
+    bool CaseVide(Coord ind) const{return GetId(ind) == -1;}
+    
+    /**
+     * @brief verifie si la case a l'indice choisi est vide
+     * 
+     * @param ind les coordonne de la case a verifier
+     * @return true si la case est vide 
+     * @return false sinon
+     */
+    void setCase(Coord ind , int val);
+    
+    /**
+     * @brief vide une case donnees dans la grille
+     * 
+     * @param a les coordonnes de la case a vider
+     */
+    void videCase(Coord a);
+    
 private:
+    
+    //attributs
+    
     Plateau grille{};
 };
 #endif //PROJETRENARDLAPIN_GRILLE_HPP
